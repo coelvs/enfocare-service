227ce79d6acdd6dcb74b1ec4739746ff1b3e3125
Spheenks <salimbaojoshua@gmail.com>
2025-03-12 03:34:50 +0800

    Admin Integration

---
+++ src/main/java/com/enfocareservice/config/AdminServerConfig.java
@@ -0,0 +1,10 @@
+package com.enfocareservice.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@Configuration
+@EnableAdminServer
+public class AdminServerConfig {
+}
