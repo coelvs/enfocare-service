9594d5485e7428b2bd4a9edde05f93139345ee07
Spheenks <salimbaojoshua@gmail.com>
2025-03-15 23:25:37 +0800

    new admin updates

--- src/main/java/com/enfocareservice/config/SecurityConfiguration.java
+++ src/main/java/com/enfocareservice/config/SecurityConfiguration.java
@@ -39,7 +39,7 @@ public class SecurityConfiguration {
 																						// Actuator
 						.requestMatchers("/api/v1/auth/**", "/enfocare/chat/ws/**").permitAll().anyRequest()
 						.authenticated())
-				.formLogin(login -> login.defaultSuccessUrl("/admin", true).permitAll())
+				.formLogin(login -> login.defaultSuccessUrl("/admin/", true).permitAll())
 				.logout(logout -> logout.logoutUrl("/admin/logout").logoutSuccessUrl("/admin/login?logout")
 						.invalidateHttpSession(true).deleteCookies("JSESSIONID").permitAll() // ✅ Ensure logout is
 																								// allowed for everyone
