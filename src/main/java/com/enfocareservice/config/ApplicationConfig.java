804a83cbb98593d2c4446462fae476b1a8036edd
Spheenks <salimbaojoshua@gmail.com>
2025-03-15 19:53:10 +0800

    new admin updates

--- src/main/java/com/enfocareservice/config/ApplicationConfig.java
+++ src/main/java/com/enfocareservice/config/ApplicationConfig.java
@@ -8,27 +8,23 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class ApplicationConfig {
 
 	@Autowired
-	private UserDetailsService userDetailsService; // Inject instead of calling userDetailsService()
+	private UserDetailsService userDetailsService; // Autowired userDetailsService
+
+	@Autowired
+	private PasswordEncoder passwordEncoder; // Autowired PasswordEncoder
 
 	@Bean
 	public AuthenticationProvider authenticationProvider() {
 		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
 		authProvider.setUserDetailsService(userDetailsService);
-		authProvider.setPasswordEncoder(passwordEncoder());
+		authProvider.setPasswordEncoder(passwordEncoder);
 		return authProvider;
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		// TODO Auto-generated method stub
-		return new BCryptPasswordEncoder();
 	}
 
 	@Bean
