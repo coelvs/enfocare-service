fef7f3a61d2c2a1e562c3cdef8f3de92fe32540c
Spheenks <salimbaojoshua@gmail.com>
2025-03-15 23:38:04 +0800

    new admin updates

--- src/main/java/com/enfocareservice/config/SecurityConfiguration.java
+++ src/main/java/com/enfocareservice/config/SecurityConfiguration.java
@@ -30,20 +30,18 @@ public class SecurityConfiguration {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
-				.csrf(csrf -> csrf.ignoringRequestMatchers("/api/**", "/actuator/**")) // ✅ Allow CSRF for form login &
-																						// actuators
+		http.cors(cors -> cors.configurationSource(corsConfigurationSource())).csrf().disable()
 				.authorizeHttpRequests(auth -> auth.requestMatchers("/admin/**").hasRole("ADMIN") // ✅ Protect admin
 																									// panel
 						.requestMatchers("/actuator/**", "/extensions/**").permitAll() // ✅ Allow Spring Boot Admin &
 																						// Actuator
 						.requestMatchers("/api/v1/auth/**", "/enfocare/chat/ws/**").permitAll().anyRequest()
 						.authenticated())
-				.formLogin(login -> login.defaultSuccessUrl("/admin/", true).permitAll())
+				.formLogin(login -> login.defaultSuccessUrl("/admin", true).permitAll())
 				.logout(logout -> logout.logoutUrl("/admin/logout").logoutSuccessUrl("/admin/login?logout")
 						.invalidateHttpSession(true).deleteCookies("JSESSIONID").permitAll() // ✅ Ensure logout is
 																								// allowed for everyone
-				).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+				).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authenticationManager(authenticationManager())
 				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
