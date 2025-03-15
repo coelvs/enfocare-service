a2314bd5745bce4cf65495f164ec5b6b42cfa184
Spheenks <salimbaojoshua@gmail.com>
2025-03-15 19:45:52 +0800

    new admin updates

--- src/main/java/com/enfocareservice/config/ApplicationConfig.java
+++ src/main/java/com/enfocareservice/config/ApplicationConfig.java
@@ -8,29 +8,19 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import com.enfocareservice.repository.UserRepository;
 
 @Configuration
 public class ApplicationConfig {
 
 	@Autowired
-	private UserRepository repository;
-
-	@Bean
-	public UserDetailsService userDetailsService() {
-
-		return username -> repository.findByEmail(username)
-				.orElseThrow(() -> new UsernameNotFoundException("User not found"));
-	}
+	private UserDetailsService userDetailsService; // Inject instead of calling userDetailsService()
 
 	@Bean
 	public AuthenticationProvider authenticationProvider() {
 		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setUserDetailsService(userDetailsService);
 		authProvider.setPasswordEncoder(passwordEncoder());
 		return authProvider;
 	}
