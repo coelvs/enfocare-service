9e6efa8e9e56174e58b531a9ed8753bd37d42813
Spheenks <salimbaojoshua@gmail.com>
2025-03-16 23:46:17 +0800

    final touches for enfocarev2

--- src/main/java/com/enfocareservice/config/SecurityConfiguration.java
+++ src/main/java/com/enfocareservice/config/SecurityConfiguration.java
@@ -37,8 +37,8 @@ public class SecurityConfiguration {
 		http.cors(cors -> cors.configurationSource(corsConfigurationSource())).csrf().disable()
 				.headers(headers -> headers.frameOptions().sameOrigin()) // ✅ Allow iframes from the same origin
 				.authorizeHttpRequests().requestMatchers("/admin/**", "/actuator/**", "/extensions/**").permitAll()
-				.requestMatchers("/api/v1/auth/**", "/enfocare/chat/ws/**").permitAll()
-				.requestMatchers("/enfocare/medical-file/**").permitAll().anyRequest().authenticated().and()
+				.requestMatchers("/api/v1/auth/**", "/enfocare/chat/ws/**", "/profile/save", "/user/profiles")
+				.permitAll().requestMatchers("/enfocare/medical-file/**").permitAll().anyRequest().authenticated().and()
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 				.authenticationProvider(authenticationProvider)
 				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
