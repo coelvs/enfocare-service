99f3b7cc765a980c589c1f74da0e6683b9a30c00
Spheenks <salimbaojoshua@gmail.com>
2025-03-17 00:08:35 +0800

    final touches for enfocarev2

--- src/main/java/com/enfocareservice/config/SecurityConfiguration.java
+++ src/main/java/com/enfocareservice/config/SecurityConfiguration.java
@@ -38,8 +38,8 @@ public class SecurityConfiguration {
 				.headers(headers -> headers.frameOptions().sameOrigin()) // ✅ Allow iframes from the same origin
 				.authorizeHttpRequests().requestMatchers("/admin/**", "/actuator/**", "/extensions/**").permitAll()
 				.requestMatchers("/api/v1/auth/**", "/enfocare/chat/ws/**", "/profile/save", "/user/profiles")
-				.permitAll().requestMatchers("/enfocare/medical-file/**").permitAll().anyRequest().authenticated().and()
-				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+				.permitAll().requestMatchers("/enfocare/medical-file/**", "/user/profiles").permitAll().anyRequest()
+				.authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 				.authenticationProvider(authenticationProvider)
 				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
 				.logout(logout -> logout.logoutUrl("/api/v1/auth/logout").addLogoutHandler(logoutHandler)
