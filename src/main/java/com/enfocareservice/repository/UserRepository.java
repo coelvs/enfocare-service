45c4905075d8cd813b218e08378e1be7cf79ccc8
Spheenks <salimbaojoshua@gmail.com>
2025-03-16 13:29:06 +0800

    fixes and update for Login and Signup

--- src/main/java/com/enfocareservice/repository/UserRepository.java
+++ src/main/java/com/enfocareservice/repository/UserRepository.java
@@ -3,8 +3,6 @@ package com.enfocareservice.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.enfocareservice.entity.UserEntity;
@@ -12,8 +10,7 @@ import com.enfocareservice.entity.UserEntity;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
-	@Query("SELECT u FROM UserEntity u WHERE u.userEmail = :email") // CORRECT ✅
-	Optional<UserEntity> findByEmail(@Param("email") String email);
+	Optional<UserEntity> findByEmail(String email);
 
 	Optional<UserEntity> findById(Integer senderId);
 
