95d0fc7f38956f8fd057c82e343d651f70a203d4
Spheenks <salimbaojoshua@gmail.com>
2025-03-18 12:06:47 +0800

    fix for Doctor Setup

--- src/main/java/com/enfocareservice/config/SecurityConfiguration.java
+++ src/main/java/com/enfocareservice/config/SecurityConfiguration.java
@@ -37,7 +37,7 @@ public class SecurityConfiguration {
 		http.cors(cors -> cors.configurationSource(corsConfigurationSource())).csrf().disable()
 				.headers(headers -> headers.frameOptions().sameOrigin()) // ✅ Allow iframes from the same origin
 				.authorizeHttpRequests().requestMatchers("/admin/**", "/actuator/**", "/extensions/**").permitAll()
-				.requestMatchers("/api/v1/auth/**", "/enfocare/chat/ws/**", "/profile/**").permitAll()
+				.requestMatchers("/api/v1/auth/**", "/enfocare/chat/ws/**", "enfocare/profile/**").permitAll()
 				.requestMatchers("/enfocare/medical-file/**").permitAll().anyRequest().authenticated().and()
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 				.authenticationProvider(authenticationProvider)
